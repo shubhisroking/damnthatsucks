16bb7481ee5079730e5adbf5dae4a6fa611d370d
Shubh <shubhisroking@proton.me>
2025-09-01 23:42:46 +0530

    damn that sucks
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- player.gd
+++ player.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -900.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +10,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 8)
 
 	move_and_slide()
+	
+	# Show or hide the fall label based on falling state
+	var fall_label = get_node("/root/Main/FallLabel")
+	if not is_on_floor() and velocity.y > 0:
+		fall_label.visible = true
+	else:
+		fall_label.visible = false
